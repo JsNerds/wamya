3eb201463d3b69e5628417857709c3a4280dcabc
SaidiAhmed11 <49171816+SaidiAhmed11@users.noreply.github.com>
2021-03-30 18:49:04 +0100

    Cust/Entrep Demo front interfaces

--- src/components/front/CustomerInterfaceBody.js
+++ src/components/front/CustomerInterfaceBody.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import EntrepriseStats from "./EntrepriseStats";
+import React, {Fragment} from 'react';
 import EntrepriseDrivers from "./EntrepriseDrivers";
 import PackageSlider from "./PackageSlider";
+import CustomerStats from "./CustomerStats";
+import CustomerFavoriteDrivers from "./CustomerFavoriteDrivers";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { Button, Tooltip } from '@material-ui/core';
+import CustomerOperationsPayments from "./CustomerOperationsPayments";
+import SendPackages from "../../pages/front/SendPackages";
+import PackagesForm from "./PackagesForm";
+
 
 const CustomerInterfaceBody =()=>{
+
     return(
         <section className="faq_area bg_color sec_pad">
             <div className="container">
@@ -17,32 +26,54 @@ const CustomerInterfaceBody =()=>{
                                 </li>
 
                                 <li className="nav-item">
-                                    <a className="nav-link" id="returns-tab" data-toggle="tab" href="#Drivers" role="tab" aria-controls="returns" aria-selected="false">My Drivers </a>
+                                    <a className="nav-link" id="returns-tab" data-toggle="tab" href="#Drivers" role="tab" aria-controls="returns" aria-selected="false">My Favorite Drivers </a>
                                 </li>
+
+                                <li className="nav-item">
+                                    <a className="nav-link" id="price-tab" data-toggle="tab" href="#SendPackage" role="tab" aria-controls="price" aria-selected="false">Send Package</a>
+                                </li>
+
 
                                 <li className="nav-item">
                                     <a className="nav-link" id="price-tab" data-toggle="tab" href="#Packages" role="tab" aria-controls="price" aria-selected="false">My Packages</a>
                                 </li>
 
+
+                                <li className="nav-item">
+                                    <a className="nav-link" id="price-tab" data-toggle="tab" href="#Payments" role="tab" aria-controls="price" aria-selected="false">My Operations and Payments</a>
+                                </li>
+
+
                             </ul>
                         </div>
                     </div>
                     <div className="col-lg-8">
+                        <Button variant="contained" color="primary" className="m-2">
+                            <FontAwesomeIcon icon={['far', 'bell']} />
+                            <span className="ml-3 badge badge-warning">
+                          <b>23</b> New
+                            </span>
+                        </Button>
+
                         <div className="tab-content faq_content" id="myTabContent">
                             <div className="tab-pane fade show active" id="Analytics" role="tabpanel" aria-labelledby="purchas-tab">
-                                    <EntrepriseStats/>
+                                    <CustomerStats/>
                             </div>
 
 
                             <div className="tab-pane fade" id="Drivers" role="tabpanel" aria-labelledby="returns-tab">
-                                    <EntrepriseDrivers/>
+                                    <CustomerFavoriteDrivers/>
                             </div>
 
                             <div className="tab-pane fade" id="Packages" role="tabpanel" aria-labelledby="price-tab">
                                     <PackageSlider/>
                             </div>
-                            <div className="tab-pane fade" id="care" role="tabpanel" aria-labelledby="care-tab">
+                            <div className="tab-pane fade" id="Payments" role="tabpanel" aria-labelledby="price-tab">
+                                    <CustomerOperationsPayments/>
+                            </div>
 
+                            <div className="tab-pane fade" id="SendPackage" role="tabpanel" aria-labelledby="price-tab">
+                                    <PackagesForm/>
                             </div>
                         </div>
                     </div>
