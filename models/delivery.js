9749370c67224015cb7b861e3367aeba96f36efa
aziz sahnoun <azizsahnoun5@gmail.com>
2021-03-31 02:17:39 +0100

    add_new_attributes_to_Delv_man

--- models/delivery.js
+++ models/delivery.js
@@ -2,10 +2,16 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var Delivery = new Schema({
+  Username: String,
+  Email: String,
+  Password: String,
   FullName: String,
   Phone: Number,
   Status: Number,
   Region: String,
+  Licence: String,
+  Gender: String,
+  Date_birth: Date,
 });
 
 module.exports = mongoose.model("delivery_man", Delivery);
