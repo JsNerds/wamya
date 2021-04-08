21f037ffa60bd1d31c8d084e25b7c29bb777ff10
khalilslama-dev <ahmedkhalil.slama@esprit.tn>
2021-04-08 13:51:07 +0100

    added cross origin ressource sharig

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 // import mongoDB
 var mongoose = require('mongoose');
 var config = require('./database/mongodb');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -35,7 +36,7 @@ mongoose
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
