964f892fc72db43ecdfbdcb5c4d0bb520e050ffa
Saidi Ahmed <ahmed.saidi@esprit.tn>
2021-05-04 23:58:24 +0100

    Some Chnages :100:

--- src/components/back/CompanyDetails.js
+++ src/components/back/CompanyDetails.js
@@ -18,7 +18,6 @@ import {
 import AddCircleTwoToneIcon from '@material-ui/icons/AddCircleTwoTone';
 
 import Chart from 'react-apexcharts';
-import {trTR} from "@material-ui/core/locale";
 
 export default function CompanyDetails(props) {
 
@@ -100,13 +99,14 @@ export default function CompanyDetails(props) {
         <Fragment>
             <Grid container spacing={4}>
                 <Grid item xs={12} lg={7}>
-                    <h5>{props.company.Denomination}'s Packages : </h5>
+                    <h5>{props.company.Denomination}'s Deliveries : </h5>
                     <hr/>
-                    <Card className="card-box mb-4">
+                    {
+                        props.company.deliveries.map((delivery,index) => (
+                    <Card className="card-box mb-4" key={index}>
                         <div className="card-header">
                             <div className="font-size-lg d-block text-truncate">
-                                <span className="text-black-50 pr-2">#32</span> Global
-                                competition
+                                <span className="text-black-50 pr-2">{index+1}</span> From  {delivery.sourceAddress.City} , {delivery.sourceAddress.State}
                             </div>
                             <div className="ml-auto d-flex align-items-center align-content-center">
                                 <Tooltip arrow title="View all participants">
@@ -117,155 +117,33 @@ export default function CompanyDetails(props) {
                             </div>
                         </div>
                         <CardContent className="p-3">
+                                    <Grid container spacing={4}>
+                                        <Grid item xs={12} lg={4} md={6}>
+                                            <div className="text-center">
+                                                <br/> To <br/>
+                                                {delivery.destinationAddress.map((destinations,index)=>(
+                                                    <>
+                                                        <span>{destinations.City} , {destinations.State}</span>
+                                                        <br/>
+                                                    </>
+                                                ))
+                                                }
+                                            </div>
+                                        </Grid>
+                                    </Grid>
+
                             <Grid container spacing={4}>
                                 <Grid item xs={12} lg={4} md={6}>
                                     <div className="text-center">
-                                        <span className="text-black-50 d-block">Users</span>
-                                        <b className="font-size-xxl">2,345</b>
-                                    </div>
-                                </Grid>
-                                <Grid item xs={12} lg={4} md={6}>
-                                    <div className="text-center">
-                                        <span className="text-black-50 d-block">Revenue</span>
-                                        <b className="font-size-xxl">$9,693</b>
-                                    </div>
-                                </Grid>
-                                <Grid item xs={12} md={12} lg={4} className="align-box-row">
-                                    <div className="w-100 mt-3 mt-md-0">
-                                        <div className="mb-1">
-                                            <span className="text-black-50 pr-1">Progress:</span>
-                                            <span className="text-danger">-12</span>
-                                        </div>
-                                        <LinearProgress variant="determinate" value={67} />
+                                        <span className="text-black-50 d-block">Number Of Packages</span>
+                                        <b className="font-size-xxl">{delivery.package.length}</b>
                                     </div>
                                 </Grid>
                             </Grid>
                         </CardContent>
                     </Card>
-                    <Card className="card-box mb-4">
-                        <div className="card-header">
-                            <div className="font-size-lg d-block text-truncate">
-                                <span className="text-black-50 pr-2">#33</span> International
-                                Event
-                            </div>
-                            <div className="ml-auto d-flex align-items-center align-content-center">
-                                <Tooltip arrow title="View all participants">
-                                    <IconButton color="primary" className="ml-3">
-                                        <AddCircleTwoToneIcon />
-                                    </IconButton>
-                                </Tooltip>
-                            </div>
-                        </div>
-                        <CardContent className="p-3">
-                            <Grid container spacing={4}>
-                                <Grid item xs={12} lg={4} md={6}>
-                                    <div className="text-center">
-                                        <span className="text-black-50 d-block">Users</span>
-                                        <b className="font-size-xxl">2,345</b>
-                                    </div>
-                                </Grid>
-                                <Grid item xs={12} lg={4} md={6}>
-                                    <div className="text-center">
-                                        <span className="text-black-50 d-block">Revenue</span>
-                                        <b className="font-size-xxl">$9,693</b>
-                                    </div>
-                                </Grid>
-                                <Grid item xs={12} md={12} lg={4} className="align-box-row">
-                                    <div className="w-100 mt-3 mt-md-0">
-                                        <div className="mb-1">
-                                            <span className="text-black-50 pr-1">Progress:</span>
-                                            <span className="text-danger">-12</span>
-                                        </div>
-                                        <LinearProgress variant="determinate" value={67} />
-                                    </div>
-                                </Grid>
-                            </Grid>
-                        </CardContent>
-                    </Card>
-                    <Card className="card-box mb-4">
-                        <List tag="div">
-                            <ListItem tag="div" className="p-0 d-block">
-                                <div className="card-header">
-                                    <div className="font-size-lg d-block text-truncate">
-                                        <span className="text-black-50 pr-2">#34</span> Hackathon
-                                    </div>
-                                    <div className="ml-auto d-flex align-items-center align-content-center">
-                                        <Tooltip arrow title="View all participants">
-                                            <IconButton color="primary" className="ml-3">
-                                                <AddCircleTwoToneIcon />
-                                            </IconButton>
-                                        </Tooltip>
-                                    </div>
-                                </div>
-                                <CardContent className="p-3">
-                                    <Grid container spacing={4}>
-                                        <Grid item xs={12} lg={4} md={6}>
-                                            <div className="text-center">
-                                                <span className="text-black-50 d-block">Users</span>
-                                                <b className="font-size-xxl">2,345</b>
-                                            </div>
-                                        </Grid>
-                                        <Grid item xs={12} lg={4} md={6}>
-                                            <div className="text-center">
-                                                <span className="text-black-50 d-block">Revenue</span>
-                                                <b className="font-size-xxl">$9,693</b>
-                                            </div>
-                                        </Grid>
-                                        <Grid item xs={12} md={12} lg={4} className="align-box-row">
-                                            <div className="w-100 mt-3 mt-md-0">
-                                                <div className="mb-1">
-                                                    <span className="text-black-50 pr-1">Progress:</span>
-                                                    <span className="text-danger">-12</span>
-                                                </div>
-                                                <LinearProgress value={43} />
-                                            </div>
-                                        </Grid>
-                                    </Grid>
-                                </CardContent>
-                            </ListItem>
-                            <Divider />
-                            <ListItem tag="div" className="p-0 d-block">
-                                <div className="card-header">
-                                    <div className="font-size-lg d-block text-truncate">
-                                        <span className="text-black-50 pr-2">#33</span> Gaming
-                                        center
-                                    </div>
-                                    <div className="ml-auto d-flex align-items-center align-content-center">
-                                        <Tooltip arrow title="View all participants">
-                                            <IconButton color="primary" className="ml-3">
-                                                <AddCircleTwoToneIcon />
-                                            </IconButton>
-                                        </Tooltip>
-                                    </div>
-                                </div>
-                                <CardContent className="p-3">
-                                    <Grid container spacing={4}>
-                                        <Grid item xs={12} lg={4} md={6}>
-                                            <div className="text-center">
-                                                <span className="text-black-50 d-block">Users</span>
-                                                <b className="font-size-xxl">1,466</b>
-                                            </div>
-                                        </Grid>
-                                        <Grid item xs={12} lg={4} md={6}>
-                                            <div className="text-center">
-                                                <span className="text-black-50 d-block">Revenue</span>
-                                                <b className="font-size-xxl text-danger">$6,465</b>
-                                            </div>
-                                        </Grid>
-                                        <Grid item xs={12} md={12} lg={4} className="align-box-row">
-                                            <div className="w-100 mt-3 mt-md-0">
-                                                <div className="mb-1">
-                                                    <span className="text-black-50">Progress:</span>
-                                                    <span className="text-warning">-5</span>
-                                                </div>
-                                                <LinearProgress value={72} />
-                                            </div>
-                                        </Grid>
-                                    </Grid>
-                                </CardContent>
-                            </ListItem>
-                        </List>
-                    </Card>
+                                ))
+                                }
                 </Grid>
 
 
