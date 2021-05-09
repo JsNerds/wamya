f808c30e902284af37281aed9f8f75a77b3b540c
khalilslama-dev <ahmedkhalil.slama@esprit.tn>
2021-05-09 15:11:23 +0100

    added payment button in form process

--- src/components/front/PackageForm/WaitingForDriverToConfirmPackage.js
+++ src/components/front/PackageForm/WaitingForDriverToConfirmPackage.js
@@ -1,89 +1,71 @@
 import React, { useState, useEffect } from "react";
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
-import {useServerApi} from '../../../hooks/useServerApi';
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { useServerApi } from "../../../hooks/useServerApi";
 import { LinearProgress } from "@material-ui/core";
 
 export default function WaitingForDriverToConfirmPackage(props) {
-    const history = useHistory()
-    const [DriverApi] = useServerApi("deliveryman/getdev/" + props.driver);
-    const [delivery,setDelivery] = useState();
-    var id= localStorage.getItem("id");
+  const history = useHistory();
+  const [DriverApi] = useServerApi("deliveryman/getdev/" + props.driver);
+  const [delivery, setDelivery] = useState();
+  var id = localStorage.getItem("id");
+  const renderPayment = () => {
+    
+    if (Math.round(props.duration / 3600) !== 0) {
+      history.push(
+        "/Payment?amount=" +
+          Math.round(props.amount.toFixed(2) * 100) +
+          "&id=" +
+          id +
+          "&userType=Customer&duration=hr" +
+          Math.round(props.duration / 3600) +
+          ":" +
+          Math.round((props.duration / 60) % 60) +
+          "min"
+      );
+    } else {
+      if (Math.round(props.duration / 3600) !== 0) {
+        history.push(
+          "/Payment?amount=" +
+            Math.round(props.amount.toFixed(2) * 100) +
+            "&id=" +
+            id +
+            "&userType=Customer&duration=hr" +
+            Math.round(props.duration / 3600) +
+            ":" +
+            Math.round((props.duration / 60) % 60) +
+            "min"
+        );
+      } else {
+        history.push(
+          "/Payment?amount=" +
+            Math.round(props.amount.toFixed(2) * 100) +
+            "&id=" +
+            id +
+            "&userType=Customer&duration=" +
+            Math.round(props.duration / 60) +
+            "min"
+        );
+      }
+    }
+  }
   const getAcceptedDelivery = async () => {
     try {
-      if(props.form)
-      {
-        const Delivery = await axios.get(
-          "http://localhost:3000/delivery/getLastDeliveryByCustomer/" + id
-        ).then(function(doc){
-          if(doc.data.state == 3)
-          {
-              if (Math.round(props.duration / 3600) !== 0) {
-                  history.push(
-                    "/Payment?amount=" +
-                      Math.round(props.amount.toFixed(2) * 100) +
-                      "&id=" +
-                      id +
-                      "&userType=Customer&duration=hr" +
-                      Math.round(props.duration / 3600) +
-                      ":" +
-                      Math.round((props.duration / 60) % 60) +
-                      "min"
-                  );
-                } else {
-                  
-                  if (Math.round(props.duration / 3600) !== 0) {
-                    history.push("/Payment?amount=" + Math.round(props.amount.toFixed(2) * 100) + "&id=" + id + "&userType=Customer&duration=hr" + Math.round(props.duration / 3600) + ":" + Math.round((props.duration / 60) % 60) + "min");
-                  }
-                  else {
-                    history.push("/Payment?amount=" + Math.round(props.amount.toFixed(2)*100) + "&id=" + id + "&userType=Customer&duration="+Math.round(props.duration / 60) +"min");
-                  }
-                }
-          }
-          else
-          {
-              console.log(doc.data)
-              setDelivery(doc.data)
-          }
-        });
+      if (props.form) {
+        const Delivery = await axios
+          .get("http://localhost:3000/delivery/getLastDeliveryByCustomer/" + id)
+          .then(function(doc) {
+           setDelivery(doc.data);
+          });
+      } else {
+        const Delivery = await axios
+          .get("http://localhost:3000/delivery/" + props.deliveryId)
+          .then(function(doc) {
+            setDelivery(doc.data)
+          });
       }
-      else{
-        const Delivery = await axios.get(
-          "http://localhost:3000/delivery/" + props.deliveryId
-        ).then(function(doc){
-          if(doc.data.state == 3)
-          {
-              if (Math.round(props.duration / 3600) !== 0) {
-                  history.push(
-                    "/Payment?amount=" +
-                      Math.round(props.amount.toFixed(2) * 100) +
-                      "&id=" +
-                      id +
-                      "&userType=Customer&duration=hr" +
-                      Math.round(props.duration / 3600) +
-                      ":" +
-                      Math.round((props.duration / 60) % 60) +
-                      "min"
-                  );
-                } else {
-                  
-                  if (Math.round(props.duration / 3600) !== 0) {
-                    history.push("/Payment?amount=" + Math.round(props.amount.toFixed(2) * 100) + "&id=" + id + "&userType=Customer&duration=hr" + Math.round(props.duration / 3600) + ":" + Math.round((props.duration / 60) % 60) + "min");
-                  }
-                  else {
-                    history.push("/Payment?amount=" + Math.round(props.amount.toFixed(2)*100) + "&id=" + id + "&userType=Customer&duration="+Math.round(props.duration / 60) +"min");
-                  }
-                }
-          }
-          else
-          {
-              console.log(doc.data)
-              setDelivery(doc.data)
-          }
-        });
-      }
-      
-       // set State
+
+      // set State
     } catch (err) {
       console.error(err.message);
     }
@@ -99,14 +81,28 @@ export default function WaitingForDriverToConfirmPackage(props) {
   }, []);
   return (
     <>
-    <div className="align-box-col d-flex justify-content-center mb-1">
+      <div className="align-box-col d-flex justify-content-center mb-1">
         <div>
-          <div className="font-weight-bold align-self-center">
-            Waiting for {DriverApi?.FullName} to Confirm the package receipt
-          </div>
+          {delivery?.state == 2 ? (<>
+            <div className="font-weight-bold align-self-center">
+              Waiting for {DriverApi?.FullName} to Confirm the package receipt
+            </div>
+            <LinearProgress color="primary" variant="indeterminate" />
+            </>
+          ) : (
+            <button
+              type="button"
+              onClick={() => {
+                renderPayment();
+              }}
+              className="btn_three"
+            >
+              Go to Payment
+            </button>
+          )}
         </div>
       </div>
-      <LinearProgress color="primary" variant="indeterminate" />
-      </>
+      
+    </>
   );
 }
