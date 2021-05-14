dfd4d2b47d224cdc1fa77db6d4b7144a076661c5
Saidi Ahmed <ahmed.saidi@esprit.tn>
2021-05-14 17:24:10 +0100

    Some Changes :100:

--- src/components/back/CustomerStat.js
+++ src/components/back/CustomerStat.js
@@ -1,12 +1,28 @@
-import React, {Fragment, useEffect, useState} from 'react';
-import { Grid, Avatar, Card, CardContent, Divider } from '@material-ui/core';
-import {useServerApi} from "../../hooks/useServerApi";
-
-
+import React, {Fragment} from 'react';
+import { Grid, Card, CardContent, Divider } from '@material-ui/core';
+import {ExampleWrapperSimple} from "../../layout-componentsBack";
+import ApexChartsArea from "../../componentsBack/ApexCharts/ApexChartsArea";
+import CustomersCharts from "./CustomersCharts";
 
 
 
 export default function CustomerStat(props) {
+    const customers = props.customers;
+    function totalPayments() {
+        return props.customers.reduce(function(total, item) {
+            total += item?.payments.length;
+
+            return total;
+        }, 0);
+    }
+
+    function totalDeliveries() {
+        return props.customers.reduce(function(total, item) {
+            total += item?.deliveries.length;
+
+            return total;
+        }, 0);
+    }
 
     return (
         <Fragment>
@@ -16,24 +32,24 @@ export default function CustomerStat(props) {
                     <Card className="card-box mb-4">
                         <div className="card-header">
                             <h4 className="font-size-lg mb-0 py-2 font-weight-bold">
-                                Statistics
+                                Informations
                             </h4>
                         </div>
                         <CardContent className="p-3">
                             <div className="d-flex align-items-center justify-content-between p-3">
                                 <div>
-                                    <b>Reports</b>
-                                    <div className="text-black-50">Monthly sales reports</div>
+                                    <b>Payments</b>
+                                    <div className="text-black-50">Number of customer's payments</div>
                                 </div>
                                 <div className="font-weight-bold text-danger font-size-xl">
-                                    2363
+                                    {totalPayments()}
                                 </div>
                             </div>
                             <Divider />
                             <div className="d-flex align-items-center justify-content-between p-3">
                                 <div>
                                     <b>User</b>
-                                    <div className="text-black-50">Visitors last week</div>
+                                    <div className="text-black-50">Number of customers</div>
                                 </div>
                                 <div className="font-weight-bold text-success font-size-xl">
                                     {props.customers?.length}
@@ -42,17 +58,23 @@ export default function CustomerStat(props) {
                             <Divider />
                             <div className="d-flex align-items-center justify-content-between p-3">
                                 <div>
-                                    <b>Sales</b>
+                                    <b>Deliveries</b>
                                     <div className="text-black-50">
-                                        Total average weekly report
+                                        Number of customer's deliveries
                                     </div>
                                 </div>
                                 <div className="font-weight-bold text-warning font-size-xl">
-                                    483
+                                    {totalDeliveries()}
                                 </div>
                             </div>
                         </CardContent>
                     </Card>
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                    <ExampleWrapperSimple sectionHeading="Stats">
+                        <CustomersCharts customers={props.customers}/>
+                    </ExampleWrapperSimple>
                 </Grid>
             </Grid>
 
--- src/components/front/EntrepriseStats.js
+++ src/components/front/EntrepriseStats.js
@@ -30,9 +30,12 @@ export default function EntrepriseStats(props) {
         },0)
     }
 
+
     const UpgradeSubscription = () => {
         history.push("/Pricing/"+props.company._id);
     }
+
+
     const chart31Options = {
         chart: {
             toolbar: {
@@ -66,48 +69,37 @@ export default function EntrepriseStats(props) {
     const chart31Data = [
         {
             name: 'Deliveries',
-            data: [47, 38, 56, 24, 45, 54, 38, 47, 38, 56, 24, 56, 24, 65]
+            data: [0,props.company.deliveries.length]
         }
     ];
 
 
+    const options = {
+        dataLabels: {
+            enabled: false
+        },
+        plotOptions: {
+            bar: {
+                horizontal: true
+            }
+        },
+        xaxis: {
+            categories: ['Payments', 'Deliveries', 'Packages']
+        }
+    };
+    const series = [
+        {
+            data: [props.company?.payments?.length, props.company?.deliveries?.length, props.company?.deliveries?.length]
+        }
+    ];
 
     return (
         <Fragment>
             <Grid container spacing={4}>
-                <Grid item xs={12} sm={6} md={4}>
-                    <Card className="card-box bg-premium-dark border-0 text-light mb-4">
-                        <CardContent className="p-3">
-                            <div className="d-flex align-items-start">
-                                <div className="font-weight-bold">
-                                    <small className="text-white-50 d-block mb-1 text-uppercase">
-                                        New Accounts
-                                    </small>
-                                    <span className="font-size-xxl mt-1">586,356</span>
-                                </div>
-                                <div className="ml-auto">
-                                    <div className="bg-white text-center text-success d-50 rounded-circle d-flex align-items-center justify-content-center">
-                                        <FontAwesomeIcon
-                                            icon={['far', 'chart-bar']}
-                                            className="font-size-xl"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="mt-3">
-                                <FontAwesomeIcon
-                                    icon={['fas', 'arrow-up']}
-                                    className="text-success mr-1"
-                                />
-                                <span className="text-success pr-1">15.4%</span>
-                                <span className="text-white-50">increase this month</span>
-                            </div>
-                        </CardContent>
-                    </Card>
-                </Grid>
 
-
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid item xs={12} sm={7} md={5}>
+                    <br/>
+                    { (new Date().getMonth() !== new Date(props.company.SubscriptionExpirationDate).getMonth()) || (new Date().getFullYear() !== new Date(props.company.SubscriptionExpirationDate).getFullYear()) ? (
                     <Card className="card-box bg-midnight-bloom text-light mb-4">
                         <CardContent className="p-3">
                             <div className="d-flex align-items-start">
@@ -131,38 +123,42 @@ export default function EntrepriseStats(props) {
                             </div>
 
                         </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Card className="card-box bg-plum-plate text-light mb-4">
-                        <CardContent className="p-3">
-                            <div className="d-flex align-items-start">
-                                <div className="font-weight-bold">
-                                    <small className="text-white-50 d-block mb-1 text-uppercase">
-                                        Orders
-                                    </small>
-                                    <span className="font-size-xxl mt-1">345</span>
-                                </div>
-                                <div className="ml-auto">
-                                    <div className="bg-white text-center text-primary d-50 rounded-circle d-flex align-items-center justify-content-center">
-                                        <FontAwesomeIcon
-                                            icon={['far', 'chart-bar']}
-                                            className="font-size-xl"
-                                        />
+                    </Card>)
+                        :(
+                            <Card className="card-box bg-danger text-light mb-4">
+                                <CardContent className="p-3">
+                                    <div className="d-flex align-items-start">
+                                        <div className="font-weight-bold">
+                                            <small className="text-light d-block mb-1 text-uppercase">
+                                                You subscription will expire this month Please Upgrade it :
+                                            </small>
+                                            <span>{props.company.SubscriptionExpirationDate.toString().substr(0,10)}</span>
+                                            <br/>
+                                            <button className="btn-warning" onClick={UpgradeSubscription}>Upgrade your subscription</button>
+                                        </div>
+
+                                        <div className="ml-auto">
+                                            <div className="bg-white text-center text-danger d-50 rounded-circle d-flex align-items-center justify-content-center">
+                                                <FontAwesomeIcon
+                                                    icon={['far', 'id-card']}
+                                                    className="font-size-xl"
+                                                />
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div className="mt-3">
-                                <FontAwesomeIcon
-                                    icon={['fas', 'arrow-down']}
-                                    className="text-white mr-1"
-                                />
-                                <span className="text-white px-1">15.4%</span>
-                                <span className="text-white-50">less orders</span>
-                            </div>
-                        </CardContent>
-                    </Card>
+
+                                </CardContent>
+                            </Card>
+                        ) }
                 </Grid>
+
+
+                <Grid item xs={12} sm={6} md={7}>
+                            <Chart options={options} series={series} type="bar" />
+                </Grid>
+
+
+
             </Grid>
 
 
