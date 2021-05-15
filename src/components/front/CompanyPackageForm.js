1e45c7ca2adc1cfa71d8192970a2d88682ab7498
khalilslama-dev <ahmedkhalil.slama@esprit.tn>
2021-05-15 21:05:45 +0100

    added some features for company

--- src/components/front/CompanyPackageForm.js
+++ src/components/front/CompanyPackageForm.js
@@ -138,6 +138,7 @@ export default function CompanyPackageForm(props) {
         Latitude: 0,
       },
       CustomerModel: "entreprise",
+      Paid:true,
     },
     onSubmit: async (values) => {
       await calculateDistance();
