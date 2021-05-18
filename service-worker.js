a661b6ead54930caad63d0ca5024de54e56b1cb1
aziz sahnoun <azizsahnoun5@gmail.com>
2021-05-18 22:25:39 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c135d9241f4c3a814d066bd9cedbd4d.js"
+  "/precache-manifest.3e00602fcea0aadf45cffa73e9c6c697.js"
 );
 
 self.addEventListener('message', (event) => {
